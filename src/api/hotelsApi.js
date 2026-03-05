2a7cea9d37b8b743cacbc54973c7e73305577980
RiyaAhurasense404 <riya.singh@ahurasense.com>
2026-03-05 17:53:14 +0530

    hotelsapi.js completed

---
+++ src/api/hotelsApi.js
@@ -0,0 +1,29 @@
+import axiosInstance from "./axiosInstance"
+
+export const fetchAutoComplete = async (query) => {
+  const response = await axiosInstance.get("/hotels/search/autocomplete", {
+    params: {
+      query: query,
+    },
+  });
+  return response.data;
+};
+
+export const fetchHotels = async ({
+  dest_id,
+  dest_type,
+  checkin_date,
+  checkout_date,
+}) => {
+  const response = await axiosInstance.get("/hotels/search/by-dest", {
+    params: {
+      dest_id,
+      dest_type,
+      checkin_date,
+      checkout_date,
+      adults: 2,
+      currency_code: "USD",
+    },
+  });
+  return response.data;
+};
