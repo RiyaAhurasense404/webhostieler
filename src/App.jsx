ca3f6b3ffe9ebc93d8ac93fbb5fcf8b2265c8295
RiyaAhurasense404 <riya.singh@ahurasense.com>
2026-03-05 11:34:59 +0530

    App completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/layout/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import HotelsPage from "./pages/HotelsPage";
+import HotelsDetailPage from "./pages/HotelsDetailPage";
+import WishListPage from "./pages/WishListPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import BookingPage from "./pages/BookingPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-
   return (
-    <>
-      <Navbar/>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/hotels" element={<HotelsPage />} />
+          <Route path="/hotels/:id" element={<HotelsDetailPage />} />
+          <Route path="/wishlist" element={<WishListPage />} />
+          <Route path="/hotels/:id/booking" element={<BookingPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
