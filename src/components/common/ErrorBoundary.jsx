86df03506f582ad8a8925c6732519ffbf3094f5b
RiyaAhurasense404 <riya.singh@ahurasense.com>
2026-03-07 13:05:43 +0530

    error handled

--- src/components/common/ErrorBoundary.jsx
+++ src/components/common/ErrorBoundary.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { Component } from "react"
 
-const ErrorBoundary = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error, info)
+  }
+
+  render() {
+    if(this.state.hasError) {
+      return <h2>Kuch galat ho gaya! 😥</h2>
+    }
+    return this.props.children
+  }
 }
 
 export default ErrorBoundary
