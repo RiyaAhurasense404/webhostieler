76c0a0e1d3fd345f0a8e825eb47a21919f069d79
RiyaAhurasense404 <riya.singh@ahurasense.com>
2026-03-11 12:02:54 +0530

    just renamed th frontend files from js to ts and jsx to tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { WishListProvider } from "./context/WishListContext.jsx";
+import { AuthProvider } from "./context/AuthContext.js";
+import { WishListProvider } from "./context/WishListContext.js";
 import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
